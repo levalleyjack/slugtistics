e1e651c9eab2ccef5779828735bdbac8f6772ab0
levalleyjack <levalleyjack@gmail.com>
2023-07-29 00:30:28 -0700

    first commit!

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,38 @@
+const AboutPage = () => {
+  return (
+    <>
+      <div id="page-body">
+        <h1>About Slugtistics</h1>
+        <h2>What is Slugtistics?</h2>
+        <p>
+          Slugtistics is an application designed to provide students and
+          educators with valuable insights into class data, grade distributions,
+          and instructor information. This webpage aims to assist students in
+          making informed decisions about their course selections to help
+          organize their courseloads and maximize their success. Slugtistics is
+          a free and open source project that is not affiliated with the
+          University of California, Santa Cruz. Slugtistics is a project created
+          by a UCSC student for UCSC students.
+        </p>
+        <h2>How did you get the data?</h2>
+        <p>
+          Slugtistic's data was obtained through the California Public Records
+          Act (CPRA) to access grade distribution information from UCSC. The
+          data obtained from the CPRA was then processed and stored in a
+          database to be used by this application.
+        </p>
+        <h2>How far does the data go back?</h2>
+        <p>Slugtistic's data goes back to Fall 2014.</p>
+        <h2>Any other questions?</h2>
+        <p>
+          Send me an email at: <b>jlevalle@ucsc.edu</b>
+        </p>
+        <p>
+          Or message me on discord at: <b>eggw</b>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ server/main.js
@@ -0,0 +1,98 @@
+import "dotenv/config";
+import express from "express";
+import sqlite3 from "sqlite3";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const app = express();
+const currentFileUrl = import.meta.url;
+const currentFilePath = fileURLToPath(currentFileUrl);
+const frontendPath = join(dirname(currentFilePath), "client/build");
+const dbPath = join(dirname(currentFilePath), "slugtistics.db");
+
+app.use(express.static(frontendPath));
+
+//===================================================================================================
+//dropdown menus
+//populating the dropdown menu with all the classes
+app.get("/SubjectCatalogNbr", (req, res) => {
+  const db = new sqlite3.Database(dbPath);
+  db.all('SELECT DISTINCT "SubjectCatalogNbr" FROM spring2022', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    } else {
+      const classTitles = rows.map((row) => row["SubjectCatalogNbr"]);
+      res.json(classTitles);
+    }
+    db.close();
+  });
+});
+
+app.get("/instructors/:subjectCatalogNbr", (req, res) => {
+  const { subjectCatalogNbr } = req.params;
+
+  const db = new sqlite3.Database(dbPath);
+  db.all(
+    'SELECT DISTINCT "Instructors" FROM spring2022 WHERE "SubjectCatalogNbr" = ?',
+    [subjectCatalogNbr],
+    (err, rows) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send("Internal Server Error");
+      } else {
+        const instructors = rows.map((row) => row["Instructors"]);
+        res.json(instructors);
+      }
+      db.close();
+    }
+  );
+});
+
+//===================================================================================================
+// actual querey from the dropdown menus
+// route to retrieve grade distribution for a selected class
+app.get("/grade-distribution/:subjectCatalogNbr", (req, res) => {
+  const { subjectCatalogNbr } = req.params;
+  const { term, instructor } = req.query;
+
+  let sqlQuery = `SELECT 
+    SUM("A+") as "A+", SUM("A") as "A", SUM("A-") as "A-", 
+    SUM("B+") as "B+", SUM("B") as "B", SUM("B-") as "B-", 
+    SUM("C+") as "C+", SUM("C") as "C", SUM("C-") as "C-", 
+    SUM("D+") as "D+", SUM("D") as "D", SUM("D-") as "D-", 
+    SUM("F") as "F"
+    FROM spring2022 WHERE "SubjectCatalogNbr" = ?`;
+
+  const params = [subjectCatalogNbr];
+
+  if (term && term !== "All") {
+    sqlQuery += " AND Term = ?";
+    params.push(term);
+  }
+
+  if (instructor && instructor !== "All") {
+    sqlQuery += " AND Instructors = ?";
+    params.push(instructor);
+  }
+
+  const db = new sqlite3.Database(dbPath);
+  db.get(sqlQuery, params, (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.json(row);
+    }
+    db.close();
+  });
+});
+
+//this was my test route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8000}`);
+});
